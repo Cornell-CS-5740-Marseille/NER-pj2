f5d40476b896dec1b1dc6e3db1bf6e1b21450c78
tracy <zw555@cornell.edu>
2018-10-13 14:21:17 -0400

    memm

--- src/MEMM.py
+++ src/MEMM.py
@@ -1,51 +1,49 @@
-NE_type = ["PER", "LOC", "ORG", "MISC"]
-feature_list = []
+from nltk.classify import MaxentClassifier
+import pickle
+class MEMM():
+    def __init__(self):
+        self.features = []
+        self.NE_type = ["PER", "LOC", "ORG", "MISC"]
+        self.word_set = ["played", "on", "American", "League"]
+        self.tag_set = ["VBD", "IN", "NNP", "NNP"]
+        self.boi_set = ["O", "O", "B-MISC", "I-MISC"]
+
+    def name_features(self, window_tuple):
+        features = {}
+        features["current_word"] = window_tuple[0]
+        features["current_tag"] = window_tuple[1]
+        return features
+    def trainMEMM(self, dump):
+        tuples = zip(self.word_set, self.tag_set, self.boi_set)
+        train_set = [(self.name_features(t), t[2]) for t in tuples]
+        self.maxent_classifier = MaxentClassifier.train(train_set, max_iter = 3)
+        if dump:
+            f = open("../models/MaxentClassifier.pickle", "wb")
+            pickle.dump(self.maxent_classifier, f)
+            f.close()
+
+    def classification(self, tuple):
+        testone = self.maxent_classifier.classify(self.name_features(tuple))
+        print(testone)
 
 
-def load_input(input):
-    word_list = []
-    tags_list = []
-    boi_list = []
-    with open(input) as input_file:
-        line_number = 0
-        for line in input_file:
-            line = line.rstrip()
-            if "------------------------" in line:
-                continue
-            if line_number % 3 == 0:
-                words = line.split("\t")
-                # print words
-            elif line_number % 3 == 1:
-                tags = line.split("\t")
-            else:
-                bois = line.split("\t")
-                word_list += words
-                tags_list += tags
-                boi_list += bois
+memm_classifier = MEMM()
+memm_classifier.trainMEMM(False)
+memm_classifier.classification(("American", "IN"))
 
-                items = zip(word_list, tags_list, boi_list, previous_BOI)
-                labeled_features.append(item)
-                previous_BOI = boi
-            line_number += 1
-
-    return (word_list, tags_list, boi_list)
-
-
-# calculate the prior (End|state) = C(state, End)/C(state)
-def prior_probability(boi_list):
-    for boi in BOI_list:
-        for j in range(len(boi_end_list)):
-            for f in boi_full_list:
-                if j == 0:
-                    if i == f:
-                        countTag = countTag + 1
-            if i == boi_end_list[j]:
-                countEnd = countEnd + 1
-    ProbE = format(countEnd / (countTag * 1.0), '.5f')
-    dicE.update({i: {"END": ProbE}})
-
-    countEnd = 0
-    countTag = 0
-
-
-print load_input("../Project2_resources/train.txt")
+# import nltk
+# nltk.download('names')
+# from nltk.corpus import names
+# import random
+# def gender_features(word):
+#    return {'last_letter':word[-1]}
+# namesljl=[(name,'male') for name in names.words('male.txt')]
+# namesljl=namesljl+[[name,'female'] for name in names.words('female.txt')]
+# random.shuffle(namesljl)
+# features=[(gender_features(n),g) for(n,g)in namesljl]
+#
+# train_set,test_set=features[5:],features[:500]
+# print train_set
+# classifier=nltk.MaxentClassifier.train(train_set, max_iter = 3)
+# testone=classifier.classify(gender_features("Neo"))
+# print testone
