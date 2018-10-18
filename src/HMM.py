bc99d2d1c44dfa550358c8e2aeeb7648144e5fc6
tx58 <tx58@cornell.edu>
2018-10-18 13:11:35 -0400

    Add annotation, cross-validation

--- src/HMM.py
+++ src/HMM.py
@@ -1,72 +1,84 @@
 from prep import prep
+import sys
+import math
+import io
 
 class HMM:
     def __init__(self):
         self.sentence_start = '<s>'
         self.sentence_end = '</s>'
 
+    def Viterbi(self, words, data):
+        '''
+        Use Viterbi algorithm to determine tags of each word.
+        input words-> words we need to tag.
+        input data-> transition_prob: transition_prob matrix.
+        input data-> generation_prob: generation_prob matrix.
+        output tag_result: the results we get from HMM algorithm. (tagging task)
+        '''
 
-
-    def Viterbi(self,data):
-        words = data[0]
         transition_prob = data[1]
         generation_prob = data[2]
         tags = ['B-PER', 'I-PER', 'B-LOC', 'I-LOC', 'B-ORG', 'I-ORG', 'B-MISC', 'I-MISC', 'O']
         tag_result_output = []
 
         for line in words:
-            score = []
-            BPTR = []
+            score = [] #'record the score of tagging'
+            BPTR = [] #'record the backward path'
 
             #initialization
             for i in range(len(tags)):
                 s = []
                 b = []
-                prob_trans = transition_prob.get(self.sentence_start, 0)
+                prob_trans = transition_prob.get(self.sentence_start, 0) #'if not exists, then set 0'
                 if prob_trans != 0:
-                    prob_trans = prob_trans.get(tags[i], 0)
-                prob_gen = generation_prob.get(tags[i], 0)
+                    prob_trans = prob_trans.get(tags[i], 0) #'if not exists, then set 0'
+                prob_gen = generation_prob.get(tags[i], 0) #'if not exists, then set 0'
                 #print prob_gen
                 if prob_gen != 0:
                     #print line[0]
-                    prob_gen = prob_gen.get(line[0], 0)
-                s.append(prob_trans*prob_gen)
-                score.append(s)
+                    prob_gen = prob_gen.get(line[0], 0)  #'if not exists, then set 0'
+                s.append((prob_trans) *(prob_gen))
                 b.append(0)
+                score.append(s)
                 BPTR.append(b)
+                # score=[ [s1], [s2], [s3], [s4]]
+                # b=[ [0], [0], [0], [0]]
 
             #iteration
-            for t in range(1, len(line)):
-                for i in range(len(tags)):
+            for t in range(1, len(line)): # location
+                for i in range(len(tags)): # tag at present tokens
                     max = -1
                     index = -1
-                    for j in range(len(tags)):
+                    for j in range(len(tags)): # tag of previous tokens
                         #prob = score[j][t-1] * transition_prob.get(tags[j], 0)
-                        prob = transition_prob.get(tags[j], 0)
-                        if prob != 0:
-                            prob = prob.get(tags[i], 0)
-                        prob = prob * score[j][t-1]
+                        tran_prob = transition_prob.get(tags[j], 0)
+                        if tran_prob != 0:
+                            prob = tran_prob.get(tags[i], 0)
+                        prob= score[i][t-1] * prob
                         if prob > max:
-                            max = prob
-                            index = j
-                    gen_prob = generation_prob.get(tags[i], 0)
-                    if gen_prob != 0:
-                        gen_prob = gen_prob.get(line[t], 0)
-                    score[i].append(max * gen_prob)
-                    BPTR[i].append(index)
+                            max = prob # Find the largest possible Viterbi(i,t)
+                            index = j  # Record the j= argmax{Viterbi(i,t)}
+                                       # =argmax{score[j][t-1]*T(j,i)*G(i,t)}=argmax{score[j][t-1]*T(j,i)}
+                        gen_prob = generation_prob.get(tags[i], 0)
+                        if gen_prob != 0:
+                            gen_prob = gen_prob.get(line[t], 0)
+                    score[i].append((max)*(gen_prob)) # Record the new score Viterbi(i,t)
+                    BPTR[i].append(index) # Record the optimal path BPTR(i,t)
 
             #identify sequence
-            tag_result_line = [None] * len(line)
+
+            tag_result_line = [None] * len(line) # Initiating tagging vector
             tag_result_line_index = [None] * len(line)
             max = -1
             index = -1
-            for i in range(len(tags)):
+            for i in range(len(tags)): # Start from the last line
                 if score[i][len(line)-1] > max:
                     max = score[i][len(line)-1]
-                    index = i
+                    index = i # Find the best tagging for the last word
             tag_result_line_index[len(line)-1] = index
             tag_result_line[len(line)-1] = tags[index]
-            for i in range(len(line)-2, 0, -1):
+            for i in range(len(line)-2, 0, -1): # Backward seek the optimal path
                 tag_result_line_index[i] = BPTR[tag_result_line_index[i+1]][i+1]
                 tag_result_line[i] = tags[tag_result_line_index[i]]
 
@@ -75,9 +87,19 @@ class HMM:
         return tag_result_output
 
 
-my_prep = prep('../Project2_resources/validation.txt')
+my_prep = prep('../Project2_resources/new_train.txt')
 data = my_prep.pre_process_hmm()
+words=[]
+with io.open("../Project2_resources/validation.txt", 'r', encoding='utf-8') as file1:
+    i=1
+    for line in file1:
+        if i % 3 == 1:
+            temp = line.split()
+            words.append(temp)   #'convert a string into a list'
+        i += 1
+
 #print data[1]
 model = HMM()
-print model.Viterbi(data)
+print(model.Viterbi(words,data))
 
+# calculate the precision and recall rate
--- src/prep.py
+++ src/prep.py
@@ -1,16 +1,23 @@
 import math
-
-
+import json
+import csv
+import pandas as pd
 class prep:
     def __init__(self, file1):
-        self.file1 = open(file1)
+        self.file1 = open(file1) #'original dataset'
         self.sentence_start = '<s>'
         self.sentence_end = '</s>'
 
     def divide_into_validation(self, validate_percent):
+        '''
+        Find validation set and training set and write to file:
+        input validate_percent: decide what percent of data will be used for validation
+        output: new_validation_file
+                new_train_file
+        '''
         line_count = 0
         sentence_count = 0
-        sentence = math.floor(1/validate_percent)
+        sentence = math.floor(1/validate_percent) #'use the mod to decide the sentence'
 
         new_train_file = open("../Project2_resources/new_train.txt", "w")
         new_validation_file = open("../Project2_resources/validation.txt", "w")
@@ -20,11 +27,37 @@ class prep:
             else:
                 new_train_file.write(line)
 
-            if line_count % 3 == 2:
+            if line_count % 3 == 2: #'At the end line of a sentence token '
                 sentence_count += 1
             line_count +=1
 
+    def cross_validation(self, fold=5):
+        '''
+        Find validation set and training set and write to file using cross validation method
+        input fold: how many folds we need, default is 5.
+        output: cross_validation_'k'
+                cross_train_file_'k'
+        '''
+        line_count = 0
+        sentence_count = 0
+
+        new_train_file = open("../Project2_resources/cross_train"+ str(k) + ".txt", "w")
+        for k in range(fold):
+            new_validation_file = open("../Project2_resources/cross_validation_" + str(k) + ".txt", "w")
+            for line in self.file1:
+                if sentence_count % sentence == k:
+                    new_validation_file.write(line)
+                else:
+                    new_train_file.write(line)
+
     def convert_table_to_prob(self, table):
+        '''
+        Convert the times matrix into probability matrix
+        input table: dictionary counting times of each index appearing
+            table[word1][word2]= times word2 appearing after word1
+        output table: dictionary calculating probability of each index
+            probtable[word1][word2]= times word2 appearing after word1/ total appearance of word1
+        '''
         for key in table:
             count = 0
             for value in table[key]:
@@ -34,6 +67,13 @@ class prep:
         return table
 
     def pre_process_hmm(self):
+        '''
+        Generate the information that HMM algorithm need
+        output sentence:
+        output transition_prob: probability probability dictionary using function convert_table_to_prob
+        output generation_prob: probability probability dictionary using function convert_table_to_prob
+
+        '''
         transition_table = {}
         generation_table = {}
         words = []
@@ -41,58 +81,74 @@ class prep:
         sentence = []
         line_count = 0
 
-        for line in self.file1:
-            if line_count % 3 == 0:
+        for line in self.file1: #'Using training dataset'
+            if line_count % 3 == 0:  #'line of original sentence'
                 words = line.split()
-                sentence.append(words)
-            elif line_count % 3 == 2:
+                sentence.append(words)   #'convert a string into a list'
+            elif line_count % 3 == 2: #'line of IOB tagging'
                 tags = line.split()
-                prev_tag = self.sentence_start
+                prev_tag = self.sentence_start   #'initiating with <s> tag'
                 for i in range(len(tags)):
                     tag = tags[i]
                     word = words[i]
-                    if prev_tag != self.sentence_start:
-                        if prev_tag in transition_table:
-                            transition_table[prev_tag][tag] = transition_table[prev_tag][tag] + 1 \
-                                if tag in transition_table[prev_tag] else 1
-                        else:
-                            transition_table[prev_tag] = {tag: 1}
-                    prev_tag = tag
-                    if tag in generation_table:
-                        generation_table[tag][word] = generation_table[tag][word] + 1 \
-                            if word in generation_table[tag] else 1
-                    else:
-                        generation_table[tag] = {word: 1}
+                    transition_table.setdefault(prev_tag, {})
+                    transition_table[prev_tag].setdefault(tag, 0)
+                    transition_table[prev_tag][tag] += 1
+                    prev_tag = tag    #'move forward, update the tag'
+                    generation_table.setdefault(tag, {})
+                    generation_table[tag][word]=generation_table[tag].setdefault(word, 0)+1
             line_count += 1
 
+        #'Finally calculate the prob matrix using appearing times'
         transition_prob = self.convert_table_to_prob(transition_table)
         generation_prob = self.convert_table_to_prob(generation_table)
+        # with open('transition_prob', 'w') as fp:
+        #     json.dump(transition_prob, fp)
+        # with open("generation_prob", 'w') as fp:
+        #     json.dump(generation_prob, fp)
+        df = pd.DataFrame(transition_prob)
+        df.to_csv('transition_prob.csv')
+        with open('generation_prob.csv', 'w') as csv_file:
+            writer = csv.writer(csv_file)
+            for key, value in generation_prob.items():
+                writer.writerow([key, value])
         return [sentence, transition_prob, generation_prob]
 
     def isCapital(self, word):
+        '''
+        Return 1 if the word is capitalized.
+        '''
         return 1 if len(word) > 0 and word[0].isupper() else 0
 
     def pre_process_memm(self):
+        '''
+        Generate features for MEMM algorithm
+        input file1: training data file
+        output:
+        '''
         output = []
         line_count = 0
 
         for line in self.file1:
-            if line_count % 3 == 0:
+            if line_count % 3 == 0: #'original sentence lists'
                 words = line.split()
-            elif line_count % 3 == 1:
+            elif line_count % 3 == 1: #'POS tag lists'
                 pos = line.split()
             else:
-                tags = line.split()
+                tags = line.split() #'IOB tag lists'
                 for i in range(len(tags)):
                     item = [words[i], pos[i], tags[i]]
+                    #'Feature Group 1: words capitilized or not'
                     feature_prev_word_cap = self.isCapital(words[i-1]) if i > 0 else 0
                     feature_curr_word_cap = self.isCapital(words[i])
                     feature_next_word_cap = self.isCapital(words[i+1]) if i < len(tags)-1 else 0
+
                     features = []
                     features.append(feature_prev_word_cap)
                     features.append(feature_curr_word_cap)
                     features.append(feature_next_word_cap)
-                    item.append(features)
+                    item.append(features) #'For each word we have several features'
+                    #'item(i) = [word(i), pos(i), tags(i), prev_word(i), ...]'
                     output.append(item)
 
             line_count += 1
